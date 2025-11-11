8ec34dc623daa7d0410b7e6c28d3f8b7ce4942f4
Pedro-HenriqueXavier <prodriguesxavier230@gmail.com>
2025-11-11 20:38:56 -0300

    att

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -44,7 +44,7 @@ export interface ErrorResponse {
   erro: string;
 }
 
-const API_URL = "http://grupo04projeto20252.escolatecnicaadelia.info/api";
+const API_URL = "https://grupo04projeto20252.escolatecnicaadelia.info/api";
 
 class ApiService {
   private baseUrl: string;
